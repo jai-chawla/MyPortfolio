99dabb7c01159f4552d64a1ec832fab54be4d0b6
jai-chawla <jaichawla074@gmail.com>
2024-08-11 22:39:31 +0530

    3rd

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='sm:mt-0 pb-8 mt-44'>
+        <div className='sm:mt-0 pb-8 mt-[250px]'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500 \\'>About</p>
         </div>
         <p className='text-xl text-justify'>I am a dedicated Full Stack Developer based in Delhi, India, with a strong academic background and a passion for creating dynamic and responsive web applications. I hold a B.Tech degree in Information Technology, where I scored a CGPA of 8.4. My technical expertise lies in modern web development technologies, including React.js, Node.js, Tailwind CSS, and MongoDB, allowing me to build robust and efficient full-stack applications.</p>
